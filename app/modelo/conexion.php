41b39a992f3f46da19e425db262a6bd990c3f636
Huepe1 <huepenico.12@gmail.com>
2019-08-13 17:04:00 -0500

    cambios realizados el día 13 de Agosto, comence a realizar el crud para actualizar el inventario

--- app/modelo/conexion.php
+++ app/modelo/conexion.php
@@ -1,12 +1,12 @@
 <?php
 
 class Conexion extends PDO{
-    function __constructor(){
+    public function __constructor(){
         try{
-            parent::__constructor("mysql:host=localhost;dbname=nombre_bd", "root", "");
+            parent::__constructor("mysql:host=localhost;dbname=RBA", "root", "");
             parent::exec("set names utf8");
-        }catch(\exception $e){
-            echo "La conexión no se pudo realizar, revise la instancia de la conexión";
+        }catch(PDOException $e){
+            echo "La conexión no se pudo realizar, revise la instancia de la conexión" . $e->getMessage();
             exit;
 
         }
